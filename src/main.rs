11f86517214f92da86c2212db09b694e8b4d9265
Charles Sibbald <casibbald@gmail.com>
2023-03-23 20:22:26 +0200

    basic dedupping tool

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use sha2::{Digest, Sha256};
+use std::collections::HashMap;
+use std::io::{self, BufRead};
+use std::time::{SystemTime};
+
+// Define the time window for deduplication
+const TIME_WINDOW: u64 = 5;
+
+fn main() {
+    // Create an empty hashmap to store seen log signatures and their last seen timestamps
+    let mut seen_signatures: HashMap<String, u64> = HashMap::new();
+
+    // Create a SHA-256 hasher
+    let mut hasher = Sha256::new();
+
+    // Create a buffer to read log lines
+    let stdin = io::stdin();
+    let reader = stdin.lock();
+    let mut lines = reader.lines();
+
+    // Loop forever, reading lines from standard input and processing them
+    loop {
+        let line = match lines.next() {
+            Some(line) => line.expect("Failed to read line"),
+            None => break, // End of input stream
+        };
+
+        // Split the line into its individual parts
+        let parts: Vec<&str> = line.split(" ").collect();
+        let _timestamp = parts[0];
+        let method = parts[1];
+        let path = parts[2];
+        let status = parts[3];
+        let bytes_sent = parts[4];
+        let referer = parts[5];
+        let user_agent = parts[6..].join(" ").trim().clone().to_owned();
+        let signature = format!(
+            "{} {} {} {} {} {}",
+            method, path, status, bytes_sent, referer, user_agent
+        );
+
+        // Hash the signature using SHA-256
+        hasher.update(signature.as_bytes());
+        let signature_hash = hasher.finalize_reset();
+        let signature_hash_hex = signature_hash.iter().map(|b| format!("{:02x}", b)).collect::<String>();
+
+        // Check if we've seen this signature hash in the last TIME_WINDOW seconds
+        let current_time = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs();
+        let last_seen_time = seen_signatures.get(&signature_hash_hex).unwrap_or(&0);
+        if current_time - last_seen_time >= TIME_WINDOW {
+            // This is a new log with a new signature, print it out
+            println!("{}", line);
+            seen_signatures.insert(signature_hash_hex, current_time);
+        } else {
+            // This is a duplicate log with a recently seen signature, increment the "seen" counter
+            seen_signatures.insert(signature_hash_hex, current_time);
+            let seen_count = seen_signatures
+                .values()
+                .filter(|&&ts| current_time - ts < TIME_WINDOW)
+                .count();
+            println!(
+                "{} (seen {} times in the last {} seconds)",
+                line, seen_count, TIME_WINDOW
+            );
+        }
+    }
+}
---
+++ tests/test_apache.rs
@@ -0,0 +1,34 @@
+use std::io::{BufReader, Write};
+use std::io::BufRead;
+use std::process::{Command, Stdio};
+
+#[test]
+fn test_deduplication() {
+    // Run the dedupfeed program as a child process
+    let mut child = Command::new("target/debug/dedupfeed")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to start dedupfeed process");
+
+    // Feed in sample Apache log file data to the program's stdin
+    let stdin = child.stdin.as_mut().expect("Failed to open stdin");
+    let file = std::fs::File::open("tests/sample_access_2.log").expect("Failed to open sample log file");
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        writeln!(stdin, "{}", line.unwrap()).expect("Failed to write to stdin");
+    }
+
+    // Collect the program's output from stdout
+    let output = child
+        .wait_with_output()
+        .expect("Failed to read child process output");
+
+    // Check that the program's output matches the expected output
+    // let expected_output = "\
+    //     127.0.0.1 - - [01/Jan/2023:00:00:01 +0000] \"GET /index.html HTTP/1.1\" 200 1234 \"-\" \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36\"\n\
+    //     127.0.0.1 - - [01/Jan/2023:00:00:02 +0000] \"GET /index.html HTTP/1.1\" 200 1234 \"-\" \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36\" (seen 1 times in the last 5 seconds)\n\
+    //     127.0.0.1 - - [01/Jan/2023:00:00:03 +0000] \"GET /index.html HTTP/1.1\" 200 1234 \"-\" \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36\" (seen 2 times in the last 5 seconds)\n\
+    // ";
+    //assert_eq!(String::from_utf8(output.stdout).unwrap(), expected_output);
+}
