207db2af3662bab1ed98831f57ed9a1f2d62ea67
Charles Sibbald <casibbald@gmail.com>
2023-03-25 13:04:50 +0200

    reverting

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,31 @@
 use sha2::{Digest, Sha256};
 use std::collections::HashMap;
-use std::io;
-use std::thread;
-use std::time::{Duration, SystemTime};
+use std::io::{self, BufRead};
+use std::time::{SystemTime};
 
 // Define the time window for deduplication
 const TIME_WINDOW: u64 = 5;
 
 fn main() {
-    let mut buffer = String::new();
-
-    loop {
-        match io::stdin().read_line(&mut buffer) {
-            Ok(n) => {
-                if n == 0 {
-                    break;
-                }
-                let data = buffer.clone();
-                thread::spawn(|| process_data(data));
-                // Clear the buffer for the next iteration
-                buffer.clear();
-            }
-            Err(e) => {
-                eprintln!("Error reading from stdin: {}", e);
-                break;
-            }
-        }
-        // need to remove the need for this sleep as it slows down ingestion.
-        thread::sleep(Duration::from_nanos(1));
-    }
-}
-
-
-
-fn process_data(data: String) {
     // Create an empty hashmap to store seen log signatures and their last seen timestamps
     let mut seen_signatures: HashMap<String, u64> = HashMap::new();
 
+    // Create a SHA-256 hasher
     let mut hasher = Sha256::new();
 
-    let lines = data.split('\n');
+    // Create a buffer to read log lines
+    let stdin = io::stdin();
+    let reader = stdin.lock();
+    let mut lines = reader.lines();
 
-    for line in lines {
-        if line.is_empty() {
-            continue;
-        }
+    // Loop forever, reading lines from standard input and processing them
+    loop {
+        let line = match lines.next() {
+            Some(line) => line.expect("Failed to read line"),
+            None => break, // End of input stream
+        };
+
+        // Split the line into its individual parts
         let parts: Vec<&str> = line.split(" ").collect();
         let _timestamp = parts[0];
         let method = parts[1];
@@ -52,7 +33,7 @@ fn process_data(data: String) {
         let status = parts[3];
         let bytes_sent = parts[4];
         let referer = parts[5];
-        let user_agent = parts[6..].join(" ").trim().to_owned();
+        let user_agent = parts[6..].join(" ").trim().clone().to_owned();
         let signature = format!(
             "{} {} {} {} {} {}",
             method, path, status, bytes_sent, referer, user_agent
